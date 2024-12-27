b6ac45635b4dc34830a5adff1aaf9f267604a479
Michał Darda <michaldarda@gmail.com>
2024-12-27 22:21:49 +0100

    Remove unused code

--- load_db.sh
+++ load_db.sh
@@ -2,11 +2,6 @@ export MYSQL_USER=otserv
 export MYSQL_PASSWORD=otserv
 export MYSQL_DATABASE=otserv
 
-# until docker-compose exec mysql mysqladmin ping -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" --silent; do
-#   echo "Waiting for MySQL to be ready..."
-#   sleep 2
-# done
-
 until docker-compose exec mysql mysql -u"$MYSQL_USER" -p"$MYSQL_PASSWORD" -e "USE $MYSQL_DATABASE"; do
   echo "Waiting for MySQL database to be ready..."
   sleep 2
